a11ab1d95b2cf8dd61376d97ac5ddf98a7a76d7e
vrrajkum <vivek.rajkumar@protonmail.com>
2018-04-18 05:15:14 -0400

    Hook App.js up to Yelp's API; v1.0 commit.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import React from 'react';
 import './App.css';
 import { BusinessList } from './components/BusinessList/BusinessList';
 import { SearchBar } from './components/SearchBar/SearchBar';
+import { Yelp } from './util/Yelp';
 
 export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      businesses: []
+    };
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
   searchYelp(term, location, sortBy) {
-    console.log(`Searching Yelp with ${term}, ${location} and ${sortBy}`)
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({
+        businesses: businesses
+      })
+    });
   }
 
   render() {
@@ -13,7 +26,7 @@ export class App extends React.Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar searchYelp={this.searchYelp} />
-        <BusinessList />
+        <BusinessList businesses={this.state.businesses} />
       </div>
     );
   }
--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -3,31 +3,25 @@ const apiKey = '8e9E3zKIOp2048ST7IH_MwOrprnN10KzVHAHV750T9tpjIrnE8i2TubOpZX3EoJa
 export const Yelp = {
   async search(term, location, sortBy) {
     try {
-      let response = await fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+      let response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
         headers: {
           Authorization: `Bearer ${apiKey}`
         }
       });
       if (response.ok) {
         let jsonResponse = await response.json();
-        return jsonResponse.businesses.map(business => return {
+        return jsonResponse.businesses.map(business => ({
           id: business.id,
           imageSrc: business.image_url,
           name: business.name,
-          address: if (business.location.address1) {
-            return address1;
-          } else if (business.location.address2) {
-            return address2;
-          } else {
-            return address3;
-          },
+          address: business.location.address1,
           city: business.location.city,
           state: business.location.state,
-          zipCode: business.location.zipCode,
+          zipCode: business.location.zip_code,
           category: business.categories[0].title,
           rating: business.rating,
           reviewCount: business.review_count
-        });
+        }));
       } else {
         throw new Error('Request Failed!');
       }
